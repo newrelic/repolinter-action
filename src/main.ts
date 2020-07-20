bd6076494b128034f852fcd740a0e5102c485c2b
Noah Koontz <nkoontz@newrelic.com>
2020-07-20 15:31:05 -0700

    integrate repolinter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {lint} from 'repolinter'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const result = lint(path.resolve('.'))
+    core.debug(JSON.stringify(result))
   } catch (error) {
     core.setFailed(error.message)
   }
