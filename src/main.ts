c7ecde07802b74aaf31e2d2af090a686ebff77fe
Noah Koontz <nkoontz@newrelic.com>
2020-07-21 09:02:06 -0700

    Put repolinter in github action

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import * as core from '@actions/core'
 import {lint} from 'repolinter'
-import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const result = lint(path.resolve('.'))
+    const result = await lint('.')
     core.debug(JSON.stringify(result))
   } catch (error) {
     core.setFailed(error.message)
