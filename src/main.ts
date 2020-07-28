17f84dbb35a3e9ef99200ecee39e319b37b4a2e3
Noah Koontz <nkoontz@newrelic.com>
2020-07-28 15:52:54 -0700

    feat: attempt to add issue-creation code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core'
-import {lint, resultFormatter} from 'repolinter'
+import * as github from '@actions/github'
+import {lint, resultFormatter, markdownFormatter} from 'repolinter'
 import getConfig from './getConfig'
 
 const INPUT_CONFIG_URL = 'config-url'
 const INPUT_CONFIG_FILE = 'config-file'
+const INPUT_TOKEN = 'token'
+const INPUT_REPO = 'repository'
 
 async function run(): Promise<void> {
   // load the configuration from file or url, depending on which one is configured
@@ -21,7 +24,28 @@ async function run(): Promise<void> {
     const result = await lint('.', undefined, true, config)
     core.debug(JSON.stringify(result))
     // print the formatted result
+    core.startGroup('Repolinter Output')
     core.info(resultFormatter.formatOutput(result, true))
+    core.endGroup()
+    // if the result is not a pass or an error, open an issue
+    // TODO: what to do if the run errors
+    // TODO: automatically create the repolinter label
+    if (!result.passed) {
+      const octokit = github.getOctokit(
+        core.getInput(INPUT_TOKEN, {required: true})
+      )
+      const [owner, repo] = core
+        .getInput(INPUT_REPO, {required: true})
+        .split('/')
+
+      await octokit.issues.create({
+        owner,
+        repo,
+        title: 'Open Source Policy Issues',
+        body: markdownFormatter.formatOutput(result, true),
+        labels: ['repolinter']
+      })
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
