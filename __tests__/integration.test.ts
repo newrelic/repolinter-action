72ddecae4b05958327f71eb08dcea5c9df6a706a
Noah Koontz <nkoontz@newrelic.com>
2020-08-13 11:47:22 -0700

    ci: update workflows and tests

--- __tests__/integration.test.ts
+++ __tests__/integration.test.ts
@@ -30,31 +30,29 @@ function getInputName(input: string): string {
   return `INPUT_${input.replace(/ /g, '_').toUpperCase()}`
 }
 
-describe('integration', () => {
-  beforeEach(() => {
-    process.env[getInputName(Inputs.REPO)] = 'newrelic/repolinter-action'
-    process.env[getInputName(Inputs.OUTPUT_TYPE)] = 'exit-code'
-    process.env[getInputName(Inputs.OUTPUT_NAME)] = 'Open Source Policy Issues'
-    process.env[getInputName(Inputs.LABEL_NAME)] = 'repolinter'
-    process.env[getInputName(Inputs.LABEL_COLOR)] = 'fbca04'
-    process.env['GITHUB_ACTION'] = 'true'
-    delete process.env[getInputName(Inputs.CONFIG_FILE)]
-    delete process.env[getInputName(Inputs.CONFIG_URL)]
-    delete process.env['GITHUB_TOKEN']
-    delete process.env['INPUT_GITHUB_TOKEN']
-    delete process.env[getInputName(Inputs.TOKEN)]
-  })
+function getBaseEnv(): NodeJS.ProcessEnv {
+  const ret: NodeJS.ProcessEnv = {}
+  ret[getInputName(Inputs.REPO)] = 'newrelic/repolinter-action'
+  ret[getInputName(Inputs.OUTPUT_TYPE)] = 'exit-code'
+  ret[getInputName(Inputs.OUTPUT_NAME)] = 'Open Source Policy Issues'
+  ret[getInputName(Inputs.LABEL_NAME)] = 'repolinter'
+  ret[getInputName(Inputs.LABEL_COLOR)] = 'fbca04'
+  ret['GITHUB_ACTION'] = 'true'
+  return ret
+}
 
+describe('integration', () => {
   // TODO: fix tests to reflect new exit code logic
   // TODO: add tests for outputs
 
   test('runs a failing test config', async () => {
-    process.env[getInputName(Inputs.CONFIG_FILE)] = path.resolve(
+    const baseEnv = getBaseEnv()
+    baseEnv[getInputName(Inputs.CONFIG_FILE)] = path.resolve(
       __dirname,
       'testconfig.json'
     )
 
-    const {out, code} = await runAction(process.env)
+    const {out, code} = await runAction(Object.assign({}, process.env, baseEnv))
 
     // console.debug(out)
     expect(code).not.toEqual(0)
@@ -64,10 +62,11 @@ describe('integration', () => {
   })
 
   test('runs a URL config', async () => {
-    process.env[getInputName(Inputs.CONFIG_URL)] =
+    const baseEnv = getBaseEnv()
+    baseEnv[getInputName(Inputs.CONFIG_URL)] =
       'https://raw.githubusercontent.com/aperture-science-incorporated/.github/master/repolinter.json'
 
-    const {out, code} = await runAction(process.env)
+    const {out, code} = await runAction(Object.assign({}, process.env, baseEnv))
 
     // console.debug(out)
     expect(code).not.toEqual(0)
@@ -78,7 +77,8 @@ describe('integration', () => {
   })
 
   test('runs the default config', async () => {
-    const {out, code} = await runAction(process.env)
+    const baseEnv = getBaseEnv()
+    const {out, code} = await runAction(Object.assign({}, process.env, baseEnv))
 
     // console.debug(out)
     expect(code).not.toEqual(0)
@@ -87,14 +87,14 @@ describe('integration', () => {
   })
 
   test('runs a passing config', async () => {
-    process.env[getInputName(Inputs.CONFIG_FILE)] = path.resolve(
+    const baseEnv = getBaseEnv()
+    baseEnv[getInputName(Inputs.CONFIG_FILE)] = path.resolve(
       __dirname,
       'passingtestconfig.json'
     )
 
-    const {out, code} = await runAction(process.env)
+    const {out, code} = await runAction(Object.assign({}, process.env, baseEnv))
 
-    // console.debug(out)
     expect(code).toEqual(0)
     expect(out).toContain('passingtestconfig.json')
     expect(out).not.toContain('undefined')
